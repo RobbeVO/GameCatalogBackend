87af49bd7e83ef6c038e8f4256b471a8a5a726b1
RobbeVO <153361978+RobbeVO@users.noreply.github.com>
2025-08-29 12:49:41 +0200

    Delete domain directory
    wrong import from main branch

--- domain/Game.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BL.domain;
-
-public class Game
-{
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public IEnumerable<Genre> Genres { get; set; }
-    public IEnumerable<Review> Reviews { get; set; }
-}
--- domain/Review.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BL.domain;
-
-public class Review
-{
-    public string Title { get; set; }
-    public string Content { get; set; }
-    public int Rating { get; set; }
-}
